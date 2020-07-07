39a813c16479b37824b4d018592561648a6f6b96
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-07 04:39:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def reverse_each_word(phrase)
   array = phrase.split(/ /)
   array.each do |array|
@@ -5,3 +6,4 @@ def reverse_each_word(phrase)
     array.reverse
   end
 end
+binding.pry
