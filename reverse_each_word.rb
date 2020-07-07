115616d5764c3ade61d850537f96746e49ed0e70
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-07 05:02:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,9 @@
 require 'pry'
 def reverse_each_word(phrase)
   array = phrase.split(" ")
-  new_array = []
-  
-  array.each do |element|
- new_array << element.reverse!
- new_array.join(" ")
+  array.collect do |element|
+ element.reverse!
   end
-  new_array = new_array.join(" ")
-  new_array
+  array = array.join(" ")
+  array
 end
